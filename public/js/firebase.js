4a860f097265bc26cd33d04a2a9a3790c3ffa973
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2023-04-25 07:30:00 -0300

    hosting adicionado

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyAbb_DU8aHE6Octp_McFG9GF3AbcKYtL6w",
+    authDomain: "todolist-53b12.firebaseapp.com",
+    projectId: "todolist-53b12",
+    storageBucket: "todolist-53b12.appspot.com",
+    messagingSenderId: "702549776689",
+    appId: "1:702549776689:web:2f35811beac08ce3233810"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
