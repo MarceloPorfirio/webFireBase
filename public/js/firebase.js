43201ae5dc3d0f455d24138f75d5c1ecdcdd5cbb
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2023-04-29 00:09:32 -0300

    link adicionado

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,6 +1,7 @@
  const firebaseConfig = {
     apiKey: "AIzaSyAbb_DU8aHE6Octp_McFG9GF3AbcKYtL6w",
     authDomain: "todolist-53b12.firebaseapp.com",
+    databaseURL: "https://todolist-53b12-default-rtdb.firebaseio.com/",
     projectId: "todolist-53b12",
     storageBucket: "todolist-53b12.appspot.com",
     messagingSenderId: "702549776689",
--- public/js/todo.js
+++ public/js/todo.js
@@ -8,6 +8,9 @@ todoForm.onsubmit = function (event) {
 
     dbRefUsers.child(firebase.auth().currentUser.uid).push(data).then(function () {
       console.log('Tarefa "' + data.name + '" adicionada com sucesso')
+    }).catch(function(error){
+      showError('Falha ao adicionar a tarefa',error)
+
     })
   } else {
     alert('O nome da tarefa não pode ser em branco para criar a tarefa!')
